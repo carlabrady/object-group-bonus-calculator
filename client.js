58670e613b6dfdb9df865af51c6f259b332ed7fd
Carla Brady <carlakaybrady@gmail.com>
2017-08-31 10:35:48 -0500

    working individual functions

--- client.js
+++ client.js
@@ -9,4 +9,54 @@ var employees = [ atticus, jem, boo, scout, robert, mayella ];
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
-console.log(employees);
+//new object constructor to create bonus info
+function EmployeeBonus (name, bonusPercentage, totalCompensation, totalBonus) {
+    this.name = name;
+    this.bonusPercentage = bonusPercentage;
+    this.totalCompensation = totalCompensation;
+    this.totalBonus = totalBonus;  
+} //end of constructor
+
+function bonusPercentageCalc(reviewRating,  employeeNumber, annualSalary) {
+    var bonusPercentage = null;
+    if (reviewRating <= 2){
+        bonusPercentage = 0;    
+        //return bonusPercentage
+     } else if (reviewRating === 3) {
+         bonusPercentage = 0.04;
+         //return bonusPercentage;
+     } else if (reviewRating === 4) {
+         bonusPercentage = 0.06;
+         //return bonusPercentage;
+     } else if (reviewRating === 5) {
+         bonusPercentage = 0.1;
+         //return bonusPercentage;
+     } 
+      if (employeeNumber.length == 4){  //might need to change string to number
+        bonusPercentage += 0.05;
+     }
+      if (annualSalary >= 65000){
+        bonusPercentage -= 0.01;
+      }
+
+     return bonusPercentage;
+}
+
+function totalBonus(annualSalary, bonusPercentage) {
+    //console.log(annualSalary * bonusPercentage);
+    return annualSalary * bonusPercentage;
+}
+
+function totalCompensation(totalBonus, annualSalary) {
+    return totalBonus + annualSalary;
+}
+
+
+
+
+
+//console.log(bonusPercentageCalc(atticus.reviewRating, atticus.employeeNumber, atticus.annualSalary));
+console.log(bonusPercentageCalc(employees[0].reviewRating, employees[0].employeeNumber , employees[0].annualSalary));
+console.log(bonusPercentageCalc(employees[1].reviewRating, employees[1].employeeNumber , employees[1].annualSalary));
+//console.log(totalCompensation());
+console.log(totalBonus(55000, 0.05));
